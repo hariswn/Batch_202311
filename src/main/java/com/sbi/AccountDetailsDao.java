f94aa404b1e088805a5b9b576f6d742e829f8ecc
Hari P <hariswn03@gmail.com>
2024-01-27 11:00:41 +0530

    code refactor

--- src/main/java/com/sbi/AccountDetailsDao.java
+++ src/main/java/com/sbi/AccountDetailsDao.java
@@ -4,17 +4,22 @@ import java.sql.*;
 
 public class AccountDetailsDao {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/mydb","root","Harry@12345");
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("select * from account_details where address = 'hyd' or address = 'mumbai';");
-        while (rs.next()) {
-            System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  "
-                    + rs.getString(3) + " " + rs.getInt(4));
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/mydb","root","Harry@12345");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from account_details where address = 'hyd' or address = 'mumbai';");
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  "
+                        + rs.getString(3) + " " + rs.getInt(4));
+            }
+            stmt.close();
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        stmt.close();
-        con.close();
+
     }
 }
